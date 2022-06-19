c630122e62cee2e9580d9c325859a8be544c881e
chelsea-banke <bankechelsea@gmail.com>
2022-06-19 10:49:19 +0100

    desktop view of sign in page complete

--- netflix/src/app/app.module.ts
+++ netflix/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
