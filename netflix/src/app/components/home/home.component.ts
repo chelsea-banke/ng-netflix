0c3e208e699938e742965bdd29eac7f40d6126cd
chelsea-banke <bankechelsea@gmail.com>
2022-06-26 13:44:06 +0100

    adding play and info button for  each movie

--- netflix/src/app/components/home/home.component.ts
+++ netflix/src/app/components/home/home.component.ts
@@ -9,17 +9,32 @@ import { TmdbService } from 'src/app/services/tmdb.service';
 })
 export class HomeComponent implements OnInit {
   Trending: any;
+  comedy: any;
+  drama: any;
+
   backdropImg: string = '';
   disInt: number = 0;
 
   constructor(private tmdbService: TmdbService,@Inject(DOCUMENT) private document: Document,@Inject('Window') private window: any) { }
 
   ngOnInit() {
+
     this.tmdbService.getTrending().then((data) => {
       this.Trending = data;
       this.disInt = Math.floor(Math.random() * (this.Trending['results'].length));
       this.backdropImg = 'https://image.tmdb.org/t/p/w1280//' + this.Trending['results'][this.disInt]['backdrop_path'];
     });
+
+    this.tmdbService.getMovies('all', 35).then((data) => {
+      this.comedy = data;
+      // console.log(this.action);
+    });
+
+    this.tmdbService.getMovies('all', 18).then((data) => {
+      this.drama = data;
+      console.log(this.drama);
+    });
+
   }
 
   scroll(dirrection: string, id: string){
--- netflix/src/app/services/tmdb.service.ts
+++ netflix/src/app/services/tmdb.service.ts
@@ -7,17 +7,23 @@ import { catchError, Observable, tap, throwError } from 'rxjs';
 })
 export class TmdbService {
   apiKey = '110dee266f1acc372caff3f7f098ea7c';
-  baseUrl = 'https://api.themoviedb.org/3//movie/' //all/?api_key=110dee266f1acc372caff3f7f098ea7c';
+  baseUrl = 'https://api.themoviedb.org/3' //all/?api_key=110dee266f1acc372caff3f7f098ea7c';
   trendingUrl = 'https://api.themoviedb.org/3/trending/all/day?api_key='
   temp: any;
 
   constructor(private http: HttpClient) { }
 
-  getMovies(sortBy: string): Promise<any>{
-    const url = this.baseUrl + sortBy + '?api_key=' + this.apiKey;
+  getMovies(sortBy: string, genre=0): Promise<any>{
+    let url: string;
+    if (genre == 0){
+      url = `${this.baseUrl}/movie//${sortBy}?api_key=${this.apiKey}`;
+    }
+    else { 
+      url = `${this.baseUrl}/discover/movie?api_key=${this.apiKey}&with_genres=${genre}`;
+    }
     const promise = new Promise((resolve, reject) => {
       this.http.get(url).subscribe(data => {
-        console.log(data);
+        // console.log(data);
         resolve(data);
       });
     })
@@ -28,7 +34,7 @@ export class TmdbService {
     const url = this.trendingUrl + this.apiKey;
     const promise = new Promise((resolve, reject) => {
       this.http.get(url).subscribe(data => {
-        console.log(data);
+        // console.log(data);
         resolve(data);
       })
     })
